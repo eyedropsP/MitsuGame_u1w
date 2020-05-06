b852cc301d7acfcdf16105159cdb18adb2eb6e0a
Toshiki Mukai <comeknight@gmail.com>
2020-05-06 22:04:13 +0900

    feat:UI関連追加

---
+++ Assets/Script/UI/UINumController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UINumController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private int currentNum;
+
+    [SerializeField]
+    Sprite[] numbers = new Sprite[10];
+
+    [SerializeField]
+    GameObject tensNumObj;
+
+    [SerializeField]
+    GameObject onesNumObj;
+
+    Image tensImg, onesImg;
+
+
+    void Start()
+    {
+        tensImg = tensNumObj.GetComponent<Image>();
+        onesImg = onesNumObj.GetComponent<Image>();
+    }
+
+    public void AddNum(int num)
+    {
+        if (currentNum + num <= 99)
+        {
+            currentNum += num;
+        }
+        else
+        {
+            currentNum = 99;
+        }
+        ApplyNum();
+    }
+
+    public void SubNum(int num)
+    {
+        if (currentNum - num >= 0) {
+            currentNum -= num;
+        }
+        else 
+        {
+            currentNum = 0;
+        }
+        ApplyNum();
+    }
+
+    public void SetNum(int num)
+    {
+        if (num <= 99)
+        {
+            currentNum = num;
+        }
+        ApplyNum();
+    }
+
+
+    void ApplyNum() {
+        int tens, ones;
+        tens = (int)Mathf.Floor(currentNum / 10);
+        ones = (int)(currentNum % 10);
+        tensImg.sprite = numbers[tens];
+        onesImg.sprite = numbers[ones];
+    }
+}
---
+++ Assets/Script/UI/UITest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public UINumController bulletNum;
+    public UINumController energyNum;
+
+
+    public void AddBullet(int num)
+    {
+        bulletNum.AddNum(num);
+    }
+
+    public void SubBullet(int num)
+    {
+        bulletNum.SubNum(num);
+    }
+
+    
+    public void AddEnergy(int num)
+    {
+        energyNum.AddNum(num);
+    }
+
+    public void SubEnergy(int num)
+    {
+        energyNum.SubNum(num);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
